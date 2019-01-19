33b013ff2065eefa82331e1f86b1b8e18a2b1e25
md kamran siddique <ksid0001@ipv6-test.local>
2019-01-20 00:14:02 +0530

    Added NextedRouting.jsx

---
+++ src/NestedRouting.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const App = () => (
+    <Router>
+        <div>
+            <header>I am Header</header>
+        </div>
+
+    </Router>
+)
+
+export default App
