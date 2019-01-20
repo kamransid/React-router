57935b1ceb700dbb4072ee01652257f509b27320
md kamran siddique <ksid0001@ipv6-test.local>
2019-01-20 09:38:11 +0530

    Adding file Nested Routing

--- src/NestedRouting.jsx
+++ src/NestedRouting.jsx
@@ -1,13 +1,80 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import {Button} from 'react-bootstrap'
 
-const App = () => (
-    <Router>
+function Home(){
+    return (
         <div>
-            <header>I am Header</header>
+            <h2>Home</h2>
         </div>
+    )
+}
 
-    </Router>
-)
+function About(){
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    )
+}
+
+function Topic({match}){
+   return (
+       <div>
+           <h3>{match.params.topicId}</h3>
+       </div>
+   )
+}
+
+function Topics({match}){
+    return (
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                <li>
+                    <Link to={`${match.url}/rendering`}>Rendring with react</Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/components`}>Components</Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/props-vs-state`}>Props vs state</Link>
+                </li>
+                
+            </ul>
+            <Route path={`${match.url}/:topicId`} component={Topic}/>
+            <Route path={match.path} exact render={()=> <h3>Please select a topic</h3>}/>
+        </div>
+    )
+}
+
+function App() {
+    return (
+
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to = '/topics'>Topics</Link>
+                    </li>
+                </ul>
+
+                <Route exact path='/' component={Home}/>
+                <Route path='/about' component={About}/>
+                <Route path='/topics' component={Topics}/>
+
+            </div>
+        </Router>
+
+
+
+    )
+}
 
 export default App
