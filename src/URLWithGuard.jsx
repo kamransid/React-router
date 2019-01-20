7d6dc12ad87e866780fd5e1b5a3462146c2aac61
md kamran siddique <ksid0001@ipv6-test.local>
2019-01-20 14:34:26 +0530

    Adding file URLWithGuard.jsx

---
+++ src/URLWithGuard.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect,
+    withRouter
+} from 'react-router-dom'
+
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true
+        setTimeout(cb, 1000)
+    },
+    signout(cb) {
+        this.isAuthenticated = false
+        setTimeout(cb, 1000)
+    }
+}
+
+
+
+
+class AuthButton extends React.Component {
+    render() {
+        if (fakeAuth.isAuthenticated) {
+            return (
+                <p>
+                    Welcome!{' '}
+                    <button onClick={() => {
+                        fakeAuth.signout(() => { this.props.history.push('/') })
+                    }}></button>
+                </p>
+            )
+        }
+        else {
+            return (
+                <h2>Sorry, You are not loged in</h2>
+            )
+        }
+    }
+}
+
+class Public extends React.Component{
+    render(){
+       return(
+           <div></div>
+       )
+    }
+}
+
+class Login extends React.Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+class PrivateRoute extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+class Protected extends React.Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+class Authentication extends React.Component {
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+            visible: true
+        }
+    }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <AuthButton />
+                    <ul>
+                        <li>
+                            <Link to='/public'>Public Page</Link>
+                        </li>
+                        <li>
+                            <Link to='/protected'>Protected Page</Link>
+                        </li>
+                    </ul>
+                    <Route path='/public' component={Public}>
+                    </Route>
+                    <Route path='/login' component={Login}></Route>
+                    <PrivateRoute path='/protected' component={Protected}></PrivateRoute>
+                </div>
+            </Router>
+        )
+    }
+
+}
+
+export default Authentication
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './Basic';
 // import App from './NestedRouting';
-import App from './URLParameters';
+// import App from './URLParameters';
+import App from './URLWithGuard';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
